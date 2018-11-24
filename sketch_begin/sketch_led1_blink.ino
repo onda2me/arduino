95c4cd9a405dfbceed439bbdafd59d8575f2415f
onda2me <onda2me@users.noreply.github.com>
2018-11-24 12:36:49 +0900

    Initial commit

---
+++ sketch_begin/sketch_led1_blink.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(13, OUTPUT);
+  
+  digitalWrite(13, HIGH); 
+  delay(1000); // 1초 delay
+  
+  digitalWrite(13, LOW);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
